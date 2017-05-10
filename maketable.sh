e9f5bf3d4474aab0fb0c6b34743cf414def131eb
Will Kirkby <will@kirk.by>
2017-05-11 00:25:22 +0100

    fix casing in maketable.sh and specified compiler in Makefile

--- maketable.sh
+++ maketable.sh
@@ -8,9 +8,9 @@ echo "#include \"table.h\"" >> table.cpp
 echo "const char* MISSING_CODEPOINT_STRING = \"<missing codepoint>\";" >> table.cpp
 echo "const char* g_unicodeTable[TABLE_LENGTH]={" >> table.cpp
 for((i=0;i<256;i++)) do
-	cut -d ':' -f 2 "unicode-table-data/loc/en/symbols/$(printf '%02x' $i)00.txt" | awk '{$1=$1;printf("\"%s\",\n", $0);}' >> table.cpp
+	cut -d ':' -f 2 "unicode-table-data/loc/en/symbols/$(printf '%02X' $i)00.txt" | awk '{$1=$1;printf("\"%s\",\n", $0);}' >> table.cpp
 done
 for((i=0;i<256;i++)) do
-	cut -d ':' -f 2 "unicode-table-data/loc/en/symbols/plane1/1$(printf '%02x' $i)00.txt" | awk '{$1=$1;printf("\"%s\",\n", $0);}' >> table.cpp
+	cut -d ':' -f 2 "unicode-table-data/loc/en/symbols/plane1/1$(printf '%02X' $i)00.txt" | awk '{$1=$1;printf("\"%s\",\n", $0);}' >> table.cpp
 done
 echo "};" >> table.cpp
